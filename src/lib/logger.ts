d10e9562f712583483a6b799fde594bae0df0d15
Prince Codes <princecodes247@gmail.com>
2025-12-29 15:50:48 +0100

    fix: circular deps in logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -153,8 +153,8 @@ export class CompositeLogger extends Logger {
 }
 
 const logger = new CompositeLogger(
-  new ConsoleLogger(env?.LOG_LEVEL),
-  new FileLogger('/logs/app.log', env?.LOG_LEVEL),
+  new ConsoleLogger((process.env?.LOG_LEVEL || LogLevel.DEBUG) as LogLevelType),
+  new FileLogger('/logs/app.log', (process.env?.LOG_LEVEL || LogLevel.DEBUG) as LogLevelType),
 )
 
 export default logger
